9d28ec47cc981b310d37e3fb001f9365baa8fe9a
pochuan chen <pochuan@gmail.com>
2018-11-05 21:14:29 +0800

    fix build break

---
+++ src/Orchard.Web/Core/Common/ViewModels/ResultViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Orchard.Core.Common.ViewModels
+{
+    public class ResultViewModel
+    {
+        public object Content { get; set; }
+        public string Code { get; set; }
+        public string Message { get; set; }
+    }
+}
