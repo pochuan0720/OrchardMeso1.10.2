21d6c69eff282f877df64f1b5c61104c45ca0854
pochuan chen <pochuan@gmail.com>
2019-01-23 15:06:39 +0800

    upload file

---
+++ src/Orchard.Web/Modules/Meso.TyMetro/ViewModels/CalendarViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TYMetro.Management.Api.Models.Time;
+
+namespace Meso.TyMetro.ViewModels
+{
+    public class CalendarViewModel
+    {
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+        public string timetable { get; set; }
+
+        public bool IsDelete { get; set; } = false;
+
+        public CalendarViewModel()
+        {
+
+        }
+
+        public CalendarViewModel(CalendarDataModel inModel)
+        {
+            Id = inModel.Id;
+            date = inModel.date;
+            timetable = inModel.timetable;
+            IsDelete = (bool)inModel.IsDelete;
+        }
+
+
+
+        public CalendarQueryModel ToQueryModel()
+        {
+            return new CalendarQueryModel { date = date, IsDelete = IsDelete };
+        }
+    }
+}
